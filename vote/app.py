b13a5aaaaf96b43da03056d0a7985a42e7c0e7da
stam237 <ahmadousop9@gmail.com>
2023-09-13 13:15:36 -0400

    vote_stam

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "DELEGUE_A")
+option_b = os.getenv('OPTION_B', "DELEGUE_B")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
